450a2a495e0d41aa4219450d27b8ea408514c433
IZUMIHARA-Natsuki <phosphinate026007@gmail.com>
2022-02-05 20:24:00 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -5,3 +5,37 @@ int main()
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+//integer
+int x;
+x = 5;
+
+//constant
+const int y = 5;
+
+//constant expression 
+constexpr int z = 7;
+
+//「const の値は実行時に決定しますが constexpr の値はコンパイル時に決定します。」
+
+//最初に書くおまじない
+#include <bits/stdc++.h>
+using namespace std;
+//main関数から始まる
+//文字列の出力
+cout << "文字列" << endl;
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+ 	cout << "こんにちは" << endl;
+	cout << "AtCoder" << endl;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "いつも" << 2525 << endl;
+  cout << "AtCoderくん" << endl;
+}
